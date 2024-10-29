bec215bec392413655ca88aa71478c630002dfc6
Musa Musa <mussamustapha0306@gmail.com>
2024-10-29 08:42:21 +0300

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <NavBar/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import NavBar from "@/components/NavBar.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "nav",
+  components: {
+    NavBar
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
